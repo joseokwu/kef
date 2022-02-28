e26fb72c92d4611eece3c279656ec9b7485413db
Atolagbe Elisha <atolagbeelisha001>
2022-02-28 17:52:02 +0100

    tailwind bug-fix

--- postcss.config.js
+++ postcss.config.js
@@ -1,25 +1,24 @@
 module.exports = {
-  plugins: {
-    theme: {
-      screens: {
-        sm: "640px",
-        // => @media (min-width: 640px) { ... }
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    screens: {
+      sm: "640px",
+      // => @media (min-width: 640px) { ... }
 
-        md: "768px",
-        // => @media (min-width: 768px) { ... }
+      md: "768px",
+      // => @media (min-width: 768px) { ... }
 
-        lg: "1024px",
-        // => @media (min-width: 1024px) { ... }
+      lg: "1024px",
+      // => @media (min-width: 1024px) { ... }
 
-        xl: "1280px",
-        // => @media (min-width: 1280px) { ... }
+      xl: "1280px",
+      // => @media (min-width: 1280px) { ... }
 
-        "2xl": "1495px",
-        "3xl": "1181px",
-        // => @media (min-width: 1536px) { ... }
-      },
+      "2xl": "1495px",
+      "3xl": "1181px",
+      // => @media (min-width: 1536px) { ... }
     },
-    tailwindcss: {},
-    autoprefixer: {},
+    extend: {},
   },
+  plugins: [],
 };
