ac0ea05345f8b3c6e1a9c2b52c3363210f97bfd5
Atolagbe Elisha <atolagbeelisha001>
2022-02-28 18:30:19 +0100

    config order fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,28 +1,29 @@
 module.exports = {
   content: ["./pages/**/*.{js,ts,jsx,tsx}", "./Components/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    screens: {
-      sm: "640px",
-      // => @media (min-width: 640px) { ... }
+    extend: {
+      screens: {
+        sm: "640px",
+        // => @media (min-width: 640px) { ... }
 
-      md: "768px",
-      // => @media (min-width: 768px) { ... }
+        md: "768px",
+        // => @media (min-width: 768px) { ... }
 
-      lg: "1024px",
-      // => @media (min-width: 1024px) { ... }
+        lg: "1024px",
+        // => @media (min-width: 1024px) { ... }
 
-      xl: "1280px",
-      // => @media (min-width: 1280px) { ... }
-      "2xl": "1495px",
-      "3xl": "1181px",
-      // => @media (min-width: 1536px) { ... }
-    },
-    extend: {},
-    backgroundImage: {
-      sidebar: "url('/sidebar-image.jpg')",
-      flare: "url(/flare.jpg)",
-      party: "url('/bg-party.jpg')",
-      "footer-texture": "url('/img/footer-texture.png')",
+        xl: "1280px",
+        // => @media (min-width: 1280px) { ... }
+        "2xl": "1495px",
+        "3xl": "1181px",
+        // => @media (min-width: 1536px) { ... }
+      },
+      backgroundImage: {
+        sidebar: "url('/sidebar-image.jpg')",
+        flare: "url(/flare.jpg)",
+        party: "url('/bg-party.jpg')",
+        "footer-texture": "url('/img/footer-texture.png')",
+      },
     },
   },
   plugins: [],
